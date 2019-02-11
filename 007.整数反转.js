1f053f08db3771ad4b767bde2f8b7befabc1ff2f
yurbin <yurbin@live.cn>
2019-02-11 15:36:53 +0800

    添加007.整数反转的实现

--- 007.整数反转.js
+++ 007.整数反转.js
@@ -23,13 +23,33 @@ var reverse = function (x) {
     const MAX_VALUE = Math.pow(2, 31) - 1;
     const MIN_VALUE = Math.pow(2, 31) * -1;
     let rev = 0;
-    while (x !== 0) {
+    while (x != 0) {
         let pop = x % 10;
-        rev *= 10;
-        x = Math.floor(x / 10);
-        rev += pop;
+        x = parseInt(x / 10);
+        if (rev > MAX_VALUE / 10 || (rev == MAX_VALUE / 10 && pop > 7)) return 0;
+        if (rev < MIN_VALUE / 10 || (rev == MIN_VALUE / 10 && pop < -8)) return 0;
+        rev = rev * 10 + pop;
     }
     return rev;
 };
 
-// console.log(reverse(123), reverse(-123), reverse(120));
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverseFirst = function (x) {
+    let digit = 0, res = 0;
+    while (x !== 0) {
+        digit = x % 10;
+        res = res * 10 + digit;
+        x = (x - digit) / 10;
+    }
+    if (res > Math.pow(2, 31) - 1 || res < Math.pow(-2, 31)) {
+        return 0;
+    } else {
+        return res;
+    }
+};
+
+console.log(reverseFirst(123), reverseFirst(-123), reverseFirst(120));
+console.log(reverse(123), reverse(-123), reverse(120));
